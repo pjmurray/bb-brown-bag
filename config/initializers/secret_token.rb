56e5ae8bb3e9f06b93345563675480462c99023c
Peter Murray <pj.murray.nz@gmail.com>
2012-03-21 20:40:46 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BbBrownBag::Application.config.secret_token = '117ce2e0fd41f017cefd7e4202c21b41424cfee62aa071bb4c9fc4a6ee12a3ec681293e173dbf0a3833de6e734607db3074c3720fbce2bf643e862a2d08c2720'
